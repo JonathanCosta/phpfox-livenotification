de3d89cd9da2ebe7d458856c62bf669a450f1b0b
ntmduy <ntmduy@gmail.com>
2015-01-02 13:09:57 +0700

    test

--- module/livenotification/template/default/block/notifications.html.php
+++ module/livenotification/template/default/block/notifications.html.php
@@ -2,5 +2,18 @@
 defined('PHPFOX') or exit('NO DICE!');
 ?>
 <div class="ln-item">
-    Someone liked something
+    <div class="fl user_image"> 
+        <div><img src="https://fbstatic-a.akamaihd.net/rsrc.php/v2/yt/r/QSjhWQt47wv.png"></div>
+    </div>
+    <div class="fl item_info">
+        <div>
+            <span>Do Trong Long liked a post you are tegged in: "Tu thuong hehe".</span>
+        </div>
+        <div>
+            <div class="fl"><img src="https://fbstatic-a.akamaihd.net/rsrc.php/v2/yt/r/QSjhWQt47wv.png"></div>
+            <div class="fl"><span>a few secons ago</span></div>
+            <div class="cl"></div>
+        </div>
+    </div>
+    <div class="cl"></div>
 </div>
