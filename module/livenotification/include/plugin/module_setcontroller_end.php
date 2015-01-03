9cc9287ea86e39b3133da4b720fe17afa4f67ac0
longdt <me@dotronglong.com>
2015-01-03 10:01:34 +0700

    apply settings to script

--- module/livenotification/include/plugin/module_setcontroller_end.php
+++ module/livenotification/include/plugin/module_setcontroller_end.php
@@ -5,6 +5,12 @@ if (phpfox::isModule('livenotification')) {
         'core.js'   => 'module_livenotification',
         'core.css'  => 'module_livenotification'
     ));
+    
+    $iDelayCheck = (int) phpfox::getParam('livenotification.notification_check');
+    $iDelayClean = (int) phpfox::getParam('livenotification.notification_clean');
+    phpfox::getLib('template')->setHeader(array(
+        '<script type="text/javascript">var ln_delay_check = '.$iDelayCheck.', ln_delay_clean = '.$iDelayClean.';</script>'
+    ));
 }
 
 ?>
--- module/livenotification/static/jscript/core.js
+++ module/livenotification/static/jscript/core.js
@@ -1,10 +1,15 @@
 var LN = {
     container: null,
     options: {
-        started: false
+        started: false,
+        delayCheck: 5000,
+        delayClean: 3000
     },
     
     init: function() {
+        this.options.delayCheck = ln_delay_check;
+        this.options.delayClean = ln_delay_clean;
+        
         if (!this.options.started) {
             this.check();
             this.options.started = true;
@@ -18,13 +23,14 @@ var LN = {
     check: function() {
         // call ajax to check if there is new notifications
         $.ajaxCall('livenotification.check');
-        setTimeout('LN.check()', 5000); // recall every 5 seconds
+        setTimeout('LN.check()', this.options.delayCheck);
     },
     attach: function(content) {
         if (content === '') return false;
+        if (!this.options.delayClean) return false;
         this.container.append(content);
         this.container.show();
-        setTimeout('LN.clean()', 3000); // clean after 3 seconds
+        setTimeout('LN.clean()', this.options.delayClean);
     },
     clean: function() {
         this.container.hide();
